d83412650812c93ed8d7a049bebca932cdf361ef
Alayode Auroraixion <chris@kayode.me>
2022-02-23 17:50:33 -0500

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,15 +19,16 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Francisco Pantoja</title>
-        <link rel="image_src" href="https://franciscopantojag.me/images/ogImage.jpg" />
+        <title>Chris Samuel | Creative Technologist
+        </title>
+        <link rel="image_src" href="https://chrissamuel.dev/images/amaze.thunderstruck.me.jpg"/>
         <meta
           name="description"
           content="Hey, this is my personal website. You can take a look at my resume, download my CV and see how you can contact me."
         />
-        <meta property="og:title" content="Francisco Pantoja" />
-        <meta property="og:site_name" content="Francisco Pantoja" />
-        <meta property="og:url" content="https://franciscopantojag.me" />
+        <meta property="og:title" content="Chris Samuel" />
+        <meta property="og:site_name" content="Chris Samuel" />
+        <meta property="og:url" content="chrissamuel.dev" />
         <meta
           property="og:description"
           content="Hey, this is my personal website. You can take a look at my resume, download my CV and see how you can contact me"
@@ -35,11 +36,11 @@ export default function Home() {
         <meta property="og:type" content="profile" />
         <meta
           property="og:image"
-          content="https://franciscopantojag.me/images/ogImage.jpg"
+          content="https://chrissamuel.dev/images/amaze.thunderstruck.me.jpg"
         />
         <meta property="og:image:width" content="1580"/>
         <meta property="og:image:height" content="790"/>
-        <meta name="twitter:image" content="https://franciscopantojag.me/images/ogImage.jpg"/>
+        <meta name="twitter:image" content="https://chrissamuel.dev/images/amaze.thunderstruck.me.jpg"/>
       </Head>
       <Modal />
       <NavBar />
